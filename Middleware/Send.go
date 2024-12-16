bf09b61460081bdc8eac0fa141c379e202ba8859
anesthesia <17621117936@163.com>
2024-12-16 09:43:29 +0800

    2024年12月16日09:27:48 1、增加数据压缩

--- Middleware/Send.go
+++ Middleware/Send.go
@@ -2,6 +2,7 @@ package Middleware
 
 import (
 	"bytes"
+	"compress/gzip"
 	"crypto/aes"
 	"crypto/cipher"
 	"crypto/rand"
@@ -12,7 +13,6 @@ import (
 	"time"
 )
 
-// ============================================ 发送数据
 // 加密数据
 func encrypt(data []byte, key []byte) ([]byte, error) {
 	block, err := aes.NewCipher(key)
@@ -31,28 +31,51 @@ func encrypt(data []byte, key []byte) ([]byte, error) {
 	return ciphertext, nil
 }
 
+// 压缩数据
+func compress(data []byte) ([]byte, error) {
+	var buf bytes.Buffer
+	writer := gzip.NewWriter(&buf)
+	if _, err := writer.Write(data); err != nil {
+		return nil, err
+	}
+	if err := writer.Close(); err != nil {
+		return nil, err
+	}
+	return buf.Bytes(), nil
+}
+
 // 发送数据到指定的 URL
 func SendData(url string, project string, data interface{}, key []byte, source string) error {
 	// 创建要发送的数据结构
 	sendData := SendDataType{
 		PROJECT:   project,
-		Data:      data, // 直接使用传入的数据，不再包装成切片
+		Data:      data,
 		Timestamp: time.Now().UnixMilli(),
-		SOURCE:    source, // 设置 bulk 字段
+		SOURCE:    source,
 	}
 
+	// 序列化数据
 	jsonData, err := json.Marshal(sendData)
 	if err != nil {
 		return err
 	}
+	log.Printf("原始数据大小: %d 字节，来源%s", len(jsonData), source) // 打印原始数据大小
 
-	//log.Printf("加密前数据：%s", string(jsonData)) // 确保以字符串形式打印
-
-	encryptedData, err := encrypt(jsonData, key)
+	// 压缩数据
+	compressedData, err := compress(jsonData)
 	if err != nil {
 		return err
 	}
+	log.Printf("压缩后数据大小: %d 字节，来源%s", len(compressedData), source) // 打印压缩后数据大小
 
+	// 加密数据
+	encryptedData, err := encrypt(compressedData, key)
+	if err != nil {
+		return err
+	}
+	log.Printf("加密后数据大小: %d 字节，来源%s", len(encryptedData), source) // 打印加密后数据大小
+
+	// 发送加密压缩后的数据
 	resp, err := http.Post(url, "application/octet-stream", bytes.NewBuffer(encryptedData))
 	if err != nil {
 		return err
@@ -64,10 +87,5 @@ func SendData(url string, project string, data interface{}, key []byte, source s
 		}
 	}(resp.Body)
 
-	//body, err := io.ReadAll(resp.Body)
-	//if err != nil {
-	//	return err
-	//}
-	//log.Printf("请求结果：%s", body)
 	return nil
 }
